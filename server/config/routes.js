ef746c3a7224c28f86aa43a92a2892be37c6a3a8
Liza Lopez <liza.b.lopez@gmail.com>
2016-02-04 19:51:50 -0800

    google oauth implemented

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,35 +8,36 @@ var mealController = require('../controllers/mealController.js');
 
 var helpers = require('./helpers.js');
 var auth = require('./authOperations.js');
-
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+var keys = require('./apiKeys.js');
+var passport = require('passport');
 
 module.exports = function(app, express) {
+
     // app.get('/foodBot/profile', userController.retrieveAllUsers); // Add AuthChecker
-	app.post('/foodBot/auth/signup', userController.signup);
+	// app.post('/foodBot/auth/signup', userController.signup);
 	app.post('/foodBot/auth/signin', userController.signin); // Add AuthChecker
-	// app.get('/foodBot/auth/signedin', userController.signedin);
+	app.get('/foodBot/auth/logout', userController.logout);
 
 	// app.post('/foodBot/recipes/', recipeController.addMeal); // Add AuthChecker
-	app.get('/foodBot/recipes/:id',  recipeController.retrieveSuggestedRecipes); // Add AuthChecker
+	app.get('/foodBot/recipes/:id', auth.checkUser, recipeController.retrieveSuggestedRecipes); // Add AuthChecker
 
-	app.get('/foodBot/meals/:id', mealController.retrieveUserMeals); // Add AuthChecker
+	app.get('/foodBot/meals/:id', auth.checkUser, mealController.retrieveUserMeals); // Add AuthChecker
 	
-	app.post('/foodBot/meals/:id', mealController.addUserMeal); // Add AuthChecker
+	app.post('/foodBot/meals/:id', auth.checkUser, mealController.addUserMeal); // Add AuthChecker
 
-	app.get('/foodBot/match/:id', matchController.retrieveMatch);
-	app.post('/foodBot/match/:id', matchController.createMatch);
-	app.delete('/foodBot/match/:id', matchController.deleteMatch);
+	app.get('/foodBot/match/:id', auth.checkUser, matchController.retrieveMatch);
+	app.post('/foodBot/match/:id', auth.checkUser, matchController.createMatch);
+	app.delete('/foodBot/match/:id', auth.checkUser, matchController.deleteMatch);
 	// app.get('/foodBot/users/home/:username', dashboardController.getUserProfile) // Add AuthChecker
 
-	app.get('/foodBot/profile/:id', profileController.retrieveOneUser); // Add 4th argument to direct to matchController, Add AuthChecker
-  app.post('/foodBot/profile/:id', profileController.addUserProfile, matchController.createMatch); // Add 4th argument to direct to matchController, Add AuthChecker
+	app.get('/foodBot/profile/:id', auth.checkUser, profileController.retrieveOneUser); // Add 4th argument to direct to matchController, Add AuthChecker
+  app.post('/foodBot/profile/:id', auth.checkUser, profileController.addUserProfile, matchController.createMatch); // Add 4th argument to direct to matchController, Add AuthChecker
   // app.put('/foodBot/profile/:id', profileController.updateUserProfile); // Add 4th argument to direct to matchController, Add AuthChecker
-  app.get('/foodBot/profile', profileController.retrieveAllUsers); //Add AuthChecker
+  app.get('/foodBot/profile', auth.checkUser, profileController.retrieveAllUsers); //Add AuthChecker
 	// app.get('/foodBot/profile/:id', auth.checkUser, profileController.retrieveOneUser); // Add 4th argument to direct to matchController
   // app.get('/foodBot/profile', auth.checkUser, profileController.retrieveAllUsers);
-	app.post('/foodBot/auth/signup', userController.signup);
-	app.post('/foodBot/auth/signin', userController.signin);
-	// app.get('/foodBot/auth/signedin', userController.signedin);
+
 
 	// app.post('/foodBot/recipes/', auth.checkUser, recipeController.addMeal); // Add AuthChecker
 	// app.get('/foodBot/recipes/:id',  auth.checkUser, recipeController.retrieveSuggestedRecipes) // Add AuthChecker
@@ -45,4 +46,64 @@ module.exports = function(app, express) {
 	// app.post('/foodBot/user/home/:id', auth.checkUser, dashboardController.addMeal); // Add AuthChecker
 	// app.get('/foodBot/users/home/:username', auth.checkUser, dashboardController.getUserProfile) // Add AuthChecker
 
+	passport.serializeUser(function(user, done) {
+		//saves user to session (req.session.passport.user = {id: '..'})
+			// console.log("serializeUser:", user)
+		done(null, user.email);
+	});
+
+	passport.deserializeUser(function(user, done) {
+		//find user by id (id, cb(err, user) {
+			// console.log("deserializeUser:", user);
+			// userController.retrieveUserByGoogleID:
+			//done (err, user);
+			//user object attaches to req as req.user
+		// })
+		done(null, user);
+	});
+
+	passport.use(new GoogleStrategy({
+		clientID: keys.google.clientID,
+		clientSecret: keys.google.clientSecret,
+		callbackURL: keys.google.callbackURL
+	},
+	function(accessToken, refreshToken, profile, done) {
+		// console.log("ACCESSING TOKEN ON CB FOR NEW GOOGLE STRAT", accessToken);
+		process.nextTick(function() {
+			//storeUser profile will not fire until we have all of our data back from Google
+			//find user based on their email
+			// var userIDFromDB = userController.storeUser(profile, accessToken, function(err,data) {
+			// 	console.log("##5 --- userIDFromDB}}}}}}}}}}", data);
+			// 	profile.DBid = data;
+			// });
+						// console.log("PROFILE after USER SAVE:", profile);
+			return done(null, profile);
+		})
+	}
+	));
+	//send to google to do the authentication
+	app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email'] }));
+	//the callback ater google has authenticated the user
+	app.get('/auth/google/callback',
+		passport.authenticate('google', {failureRedirect: '/foodBot/auth/google' }),
+		function(req, res) {
+			// console.log("##3 --- INSIDE PASSPORT AUTHENTICATE:", req.user);
+			// console.log("##3#### --- TOKEN???:", req.user.token);
+			var userObj = {};
+			userController.storeUser(req.user, function(err, userData) {
+				if (err) {
+					res.json(err);
+				} else {
+				console.log("##6 -- <<<<USERDATA FROM google/callback>>>>>", userData);
+				userObj = {
+					name: req.user.displayName,
+					id: userData.id,
+					photos: req.user.photos[0].value
+				}
+				req.DBid = userObj.id;
+				res.json(userObj);		
+				}
+			});
+
+		});
 };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,16 +37,56 @@ module.exports = {
   //   
     // });
   },
-  retrieveOneUser: function(req, res) {
+  storeUser: function(profile, next) {
+    // console.log("##1 --- USER OBJECT:", profile);
+    var userObj = {
+      name: profile.displayName,
+      email: profile.email,
+      photo: profile.photos[0].value,
+      googleID: profile.id,
+    }
+    // console.log("##2 -- CREATED USER OBJ AFTER PROFILE", userObj);
+
     var client = new pg.Client(connectionString);
     client.connect();
-    var query = client.query("SELECT * FROM Users WHERE id = "+req.params.id+";");
+    var checkUserQuery = client.query("SELECT * FROM Users where email ='"+userObj.email+"';", function(err, checkUserData) {
+      if (err) {
+        // res.status(500).json("We're sorry, an error has occurred");
+        next("We're sorry, an error has occurred", null);
+      } else if (checkUserData.rows.length > 0) {
+        // res.status(400).json('User with that email already exists');
+        userObj.id = checkUserData.rows[0].id;
+        next(null, userObj);
+      } else {
+        var createUserQuery = client.query("INSERT INTO Users (name, email, photo, googleID) VALUES ('"+userObj.name+"','"+userObj.email+"','"+userObj.photo+"','"+userObj.googleID+"') RETURNING id;", function(err, createUserData) {
+          if (err) {console.log ("ERROR IN CREATE USER", err)}
+            // console.log("##4 -- DATA RETURNED>>>>", createUserData);
+            // next(null, createUserData.rows[0].id);
+        userObj.id = createUserData.rows[0].id;
+        // console.log("PROFILE IN USERCREATION :::::::::::::::", profile);
+        // console.log("USER DATA>>>>>>:", userData)
+        next(null, userObj);
+      });
+      }
+    });
+  },
+  retrieveUserByGoogleID: function(gID, next) {
+
+  },
+  retrieveOneUser: function(email, next) {
+    var id = {};
+    var client = new pg.Client(connectionString);
+    client.connect();
+    var query = client.query("SELECT id FROM Users WHERE email = '"+email+"';");
     query.on('row', function(data) {
-      res.status(200).json(data);
+      id.id = data;
+      // res.status(200).json(data);
+      // console.log('##5 -- USER ID ^^^^^^^^^^^^^^^:', id); 
+      next(null, id);
     });
     query.on('end', function() {
       //if no id  was found and res.status was not set, declare error to client
-      res.status(400).json("USER does not exist");      
+      // res.status(400).json("USER does not exist");      
     });
   },
   retrieveAllUsers: function(req, res) {
@@ -69,10 +109,10 @@ module.exports = {
     client.query("SELECT * FROM Users where email ='"+req.body.email+"' AND password = crypt('"+req.body.password+"', password);", function(err, data) {
      if (err) {
        res.status(500).json("We're sorry, an error has occurred");
-       console.log('500');
+       // console.log('500');
      } else if (data.rows.length < 1) {
        res.status(400).json("Username or password is incorrect");
-       console.log('400');
+       // console.log('400');
      } else {
        var id = data.rows[0].id
        var userData = {
@@ -105,18 +145,23 @@ module.exports = {
         var matchQuery = client.query("SELECT * FROM USERRECIPES WHERE profileid IN (SELECT match from profiles where id='"+id+"');")
           var matchRec = allUserData.matchData.recipes
           matchQuery.on('row', function(data) {
-            console.log(data, "MATCHED FOOLS")
+            // console.log(data, "MATCHED FOOLS");
             if(!data.created && data.liked)
             matchRec.push(data)
           })
 
         matchQuery.on('end', function(data) {                      
-          console.log('matchdata sent is', allUserData.matchData)
+          // console.log('matchdata sent is', allUserData.matchData)
           res.send(allUserData)
          });
 
         }
       })
+  },
+  logout: function(req, res) {
+    req.session.destroy(function() {
+      res.redirect('/foodBot/auth/signin');
+    })
   }
 }
 
